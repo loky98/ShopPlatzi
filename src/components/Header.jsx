c68e02d5d97bf9de89276f64efafc316c4c64751
Diego Camacho <khasfklsdgkk@gmail.com>
2022-02-25 13:15:43 -0500

    se agragaron los documentos necesarios para empesar a unir todos los archivos y es el primer push a GitHub

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import '@styles/Header.scss';
+
+import menuu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import shopingCard from '@icons/icon_shopping_cart.svg'
 
 const Header = () => {
     return (
         <nav>
-            <img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+            <img src={menuu} alt="menu" className="nav_menu" />
             <div className="navbar-left">
-                <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+                <img src={logo} alt="logo" className="nav_logo" />
                 <ul>
                     <li>
                         <a href="/">All</a>
@@ -31,7 +36,7 @@ const Header = () => {
                 <ul>
                     <li className="navbar-email">platzi@example.com</li>
                     <li className="navbar-shopping-cart">
-                        <img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+                        <img src={shopingCard} alt="shopping cart" />
                         <div>2</div>
                     </li>
                 </ul>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,10 +12,16 @@ const App = () => {
         <BrowserRouter>
             <Layout>
                 <Switch>
-                    <Route exact path="/login" component={Login} />
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
                     <Route exact path="/recovery-password" component={RecoveryPasaword} />
-                    <Route component={NotFound} />
+                    <Route exact path="send-email" component={SendEmail} />
+                    <Route exact path="new-password" component={NewPassword} />
+                    <Route exact path="my-acount" component={MyAcount} />
+                    <Route exact path="create-acount" component={CreateAcount} />
+                    <Route exact path="checkout" component={Checkout} />
+                    <Route ecact pacth="orders" component={orders} />
+                    <Route path={"*"} component={NotFound} />
                 </Switch>
             </Layout>
         </BrowserRouter>
